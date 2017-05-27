252c39c07d600df76b274b6720bcfe3e7403dc09
Robert Schmid <r.schmid@outlook.com>
2017-05-27 18:33:24 +0200

    Add command line options

--- Program.cs
+++ Program.cs
@@ -1,52 +1,73 @@
-using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Logging;
 
 namespace SimpleHttpServer
 {
-    internal class Startup
-    {
-        internal static string Dir;
-
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddDirectoryBrowser();
-        }
-
-        public void Configure(IApplicationBuilder app, ILoggerFactory loggerFactory)
-        {
-            loggerFactory.AddConsole();
-            
-            var options = new FileServerOptions()
-            {
-                FileProvider = new PhysicalFileProvider(Dir),
-                RequestPath = new PathString(""),
-                EnableDirectoryBrowsing = true
-            };
-            options.StaticFileOptions.ServeUnknownFileTypes = true;
-            app.UseFileServer(options);
-        }
-    }
     public class Program
     {
         public static void Main(string[] args)
         {
-            Startup.Dir = args[0];
+            // Default options
+            var contentRoot = Directory.GetCurrentDirectory();
+            var port = 8000;
+
+            // Parse arguments
+            var remainingArgs = args.ToList();
+            while (remainingArgs.Count > 0)
+            {
+                if (Directory.Exists(remainingArgs.First()))
+                {
+                    contentRoot = remainingArgs.First();
+                } 
+                else if (int.TryParse(remainingArgs.First(), out int customPort))
+                {
+                    port = customPort;
+                }
+                else
+                {
+                    break;
+                }
+
+                remainingArgs = remainingArgs.Skip(1).ToList();
+            }
 
             var host = new WebHostBuilder()
+                .UseUrls("http://*:" + port)
+                .UseContentRoot(contentRoot)
                 .UseKestrel()
                 .UseStartup<Startup>()
                 .Build();
 
             host.Run();
+        }
+    }
+
+    internal class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDirectoryBrowser();
+        }
+
+        public void Configure(IApplicationBuilder app, ILoggerFactory loggerFactory, IHostingEnvironment hostingEnvironment)
+        {
+            loggerFactory.AddConsole();
+            
+            var options = new FileServerOptions()
+            {
+                FileProvider = hostingEnvironment.ContentRootFileProvider,
+                RequestPath = new PathString(string.Empty),
+                EnableDirectoryBrowsing = true
+            };
+
+            options.StaticFileOptions.ServeUnknownFileTypes = true;
+
+            app.UseFileServer(options);
         }
     }
 }
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# SimpleHttpServer
+# SimpleHttpServer for .NET Core
+
+Install with npm: `npm install -g dotnet-simple-http-server`.
+
+Then run `dotnet SimpleHttpServer 8000`, where the port argument is optional.
