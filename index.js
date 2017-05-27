ab9592d480f07eb6b3964295848808d2d8ff711f
Robert Schmid <r.schmid@outlook.com>
2017-05-27 15:33:52 +0200

    Rename executable to be used with dotnet cli

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 const spawn = require('child_process').spawn
 
-const dotnet = spawn('dotnet', ['run', '-c', 'Release', process.cwd()], { cwd: __dirname });
+const dotnet = spawn('dotnet', ['run', '-c', 'Release', process.cwd(), ...process.argv], { cwd: __dirname });
 
 dotnet.stdout.pipe(process.stdout);
 dotnet.stderr.pipe(process.stderr);
